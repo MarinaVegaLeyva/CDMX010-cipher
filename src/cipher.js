b0a68af0f75fa02ee4e9e9e323dc73d41a16cb8e
MarinaVegaLeyva <marina.vega.condor@gmail.com>
2020-12-15 20:53:36 -0600

    primer commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,37 @@
-const cipher = {
+/*const cipher = {
   // ...
+};*/
+
+const cipher = {
+  encode: function(offset, string) {
+    // eslint-disable-next-line no-console
+    let newString=""; 
+    for (let i = 0; i < string.length; i++) {		
+      let codeAscii=string.charCodeAt(i);
+      let newAscii=(codeAscii-65+offset)%26 +65;
+      newString+=String.fromCharCode(newAscii);
+    }
+      //console.log(newString);
+      return newString;
+  },
+  decode: function (offset,string){
+
+    let newString=""; 
+    for (let i = 0; i < string.length; i++) {
+      let codeAscii=string.charCodeAt(i);
+      //let formula = (ascii - 65 + (offset - 1)) % 26 + 65;
+      //let newAscii=(ascii - 65 + (offset - 1)) % 26 + 65;
+      let newAscii=(codeAscii+65-offset)%26 +65;
+      newString+=String.fromCharCode(newAscii);
+    }
+      //console.log(newString);
+      return newString;
+  },
+  lavida:(mensaje)=>{
+    let mmmmm=mensaje+'jajajja'
+    return `jajaja ${mmmmm}`
+  }
 };
+
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+//cipher.encode(3,"ABC");
+//cipher.decode(3,"DEF");
+
+document.querySelector("#encodebtn").addEventListener('click', function() {
+    //let offsetTxt=parseInt(document.getElementById('offset').value);
+    let offsetTxt=parseInt(document.querySelector("#offset").value); 
+    let stringTxt=document.querySelector("#string").value;
+    document.querySelector("#viewOptions").style.display="none";
+    document.querySelector("#viewDate").style.display="block";
+    let divCurp=document.querySelector(".curp") //esto es html el cual se accede desde el DOM
+    let stringEncode=cipher.encode(offsetTxt,stringTxt);
+    //console.log(stringEncode);
+    divCurp.innerHTML=`<h2>${stringEncode}</h2>` //aqui se añáde HTML al DOM, el texto viene de la variable llamada ALGO
+})
+
+let returnView= function(){
+    location.reload();
+}
+
+document.querySelector("#btnReturn").addEventListener('click',returnView);
+
+let functDecode=function(){
+    let offsetTxt=parseInt(document.querySelector("#offset").value); 
+    let stringTxt=document.querySelector("#string").value;
+    document.querySelector("#viewOptions").style.display="none";
+    document.querySelector("#viewDate").style.display="block";
+    let divCurp=document.querySelector(".curp") 
+    let stringEncode=cipher.decode(offsetTxt,stringTxt);
+    divCurp.innerHTML=`<h2>${stringEncode}</h2>`
+}
+
+document.querySelector("#decodebtn").addEventListener('click',functDecode);
+
+// let alfalfa=document.querySelector(".rayos") //esto es html el cual se accede desde el DOM
+// let algo=cipher.lavida(" Asi se rie malefica") //Esto es el valor del RETUN 
+// alfalfa.innerHTML=`<h1>${algo}</h1>` //aqui se añáde HTML al DOM, el texto viene de la variable llamada ALGO
+
+// en HTML debe haber un <div class="rayos"></div>
+
+
